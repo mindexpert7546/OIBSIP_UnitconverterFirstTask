e84d0f42febb03bc7a4850c9d698c81cb302852e
mindexpert7546 <kk7615117@gmail.com>
2023-05-28 00:43:35 +0530

    first commit

---
+++ app/src/main/java/com/kundan/unitconverter/MainActivity.java
@@ -0,0 +1,92 @@
+package com.kundan.unitconverter;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar action_bar = getSupportActionBar();
+        if (action_bar != null) {
+            action_bar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.purple_500)));
+        }
+
+        CardView area_card, length_card, temperature_card, speed_card, frequency_card, mass_card;
+
+        area_card = findViewById(R.id.cardView_area);
+        length_card = findViewById(R.id.cardView_length);
+        temperature_card = findViewById(R.id.cardView_temperature);
+        speed_card = findViewById(R.id.cardView_speed);
+        frequency_card = findViewById(R.id.cardView_frequency);
+        mass_card = findViewById(R.id.cardView_mass);
+
+        area_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AreaActivity.class));
+            }
+        });
+
+        length_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LengthActivity.class));
+            }
+        });
+
+        temperature_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TemperatureActivity.class));
+            }
+        });
+
+        speed_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SpeedActivity.class));
+            }
+        });
+
+        frequency_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, FrequencyActivity.class));
+            }
+        });
+
+        mass_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MassActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_setting) {
+            startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
